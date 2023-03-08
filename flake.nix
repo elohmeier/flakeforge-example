383a3ea4c33ea44d60dd88b6123a413444d49c56
Enno Richter <enno@nerdworks.de>
2023-03-08 13:57:51 +0100

    initial

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "flakehub example";
+
+  inputs = {
+    flakehub.url = "github:elohmeier/flakehub";
+    flakehub.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  };
+
+  outputs = { self, flakehub, nixpkgs }: {
+
+    packages.aarch64-linux.my-bash-image = flakehub.packages.aarch64-linux.flakehubTools.streamLayeredImageConf {
+      name = "bash-stream-layered";
+      contents = [ nixpkgs.legacyPackages.aarch64-linux.bashInteractive ];
+    };
+
+  };
+}
